e605663cfbda90018a102883c9335950981997b7
Neviton Viana <nevitonviana@hotmail.com>
2023-11-30 19:15:47 -0300

    construção do service player e da customização de um text

---
+++ lib/features/genre_list/data/repositories/genre_list_repository.dart
@@ -0,0 +1,8 @@
+import '../../../../core/services/api_service.dart';
+
+class GenreListRepositury {
+  final ApiService _apiService;
+
+  GenreListRepositury({required ApiService apiService})
+      : _apiService = apiService;
+}
