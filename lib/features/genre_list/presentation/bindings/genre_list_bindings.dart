e3576714ddce0c82dec2be2a27fc4d1e172a5b60
Neviton Viana <nevitonviana@hotmail.com>
2023-11-30 19:34:15 -0300

    construção da controlle de listage de generos

--- lib/features/genre_list/presentation/bindings/genre_list_bindings.dart
+++ lib/features/genre_list/presentation/bindings/genre_list_bindings.dart
@@ -1,10 +1,18 @@
 import 'package:get/get.dart';
 
+import '../../../../core/services/api_service.dart';
+import '../../data/repositories/genre_list_repository.dart';
 import '../controllers/genre_list_controller.dart';
 
 class GenreListBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(GenreListController());
+    Get.put(
+      GenreListController(
+        GenreListRepository(
+          apiService: Get.find<ApiService>(),
+        ),
+      ),
+    );
   }
 }
--- lib/features/genre_list/presentation/controllers/genre_list_controller.dart
+++ lib/features/genre_list/presentation/controllers/genre_list_controller.dart
@@ -1,7 +1,41 @@
+// ignore_for_file: unnecessary_overrides
+
 import 'package:get/get.dart';
 
+import '../../../../core/errors/failures.dart';
 import '../../../../core/mixins/screen_loading_and_error_mixin.dart';
+import '../../../../shared/models/genre_model.dart';
+import '../../data/repositories/genre_list_repository.dart';
 
-class GenreListController extends GetxController with ScreenLoadingAndErrorMixin{
+class GenreListController extends GetxController
+    with ScreenLoadingAndErrorMixin {
+  final GenreListRepository _genreListRepository;
 
+  GenreListController(GenreListRepository genreListRepository)
+      : _genreListRepository = genreListRepository;
+
+  final RxList<GenreModel> genres = RxList([]);
+
+  // ignore: unnecessary_overrides
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  Future<void> getGenreList() async {
+    setLoadingToTrue();
+    setError(null);
+    final getGenreResponse = await _genreListRepository.getGenreList();
+    getGenreResponse.fold(
+      (Failure failureResponse) {
+        if (failureResponse is GetGenreListFailure) {
+          setError(failureResponse.message);
+        }
+      },
+      (List<GenreModel> genreResponse) {
+        genres.value = genreResponse;
+      },
+    );
+    setLoadingToFalse();
+  }
 }
