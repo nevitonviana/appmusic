f7ec4030897466b2028f0deaf101d16d36ee4eaf
Neviton Viana <nevitonviana@hotmail.com>
2023-12-15 19:32:16 -0300

    criação do genre details controller

--- lib/features/genre_details/presentation/bindings/genre_details_bindings.dart
+++ lib/features/genre_details/presentation/bindings/genre_details_bindings.dart
@@ -1,11 +1,20 @@
 import 'package:get/get.dart';
 
+import '../../../../core/services/api_service.dart';
+import '../../../../shared/features/music_app/presentation/controllers/music_player_controller.dart';
+import '../../data/repositories/genre_details_repository.dart';
 import '../controllers/genre_details_controller.dart';
 
-class GenreDetailsBindings extends Bindings{
+class GenreDetailsBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(GenreDetailsController);
+    Get.put(
+      GenreDetailsController(
+        genreDetailsRepository: GenreDetailsRepository(
+          apiService: Get.find<ApiService>(),
+        ),
+        musicPlayerController: Get.find<MusicPlayerController>(),
+      ),
+    );
   }
-
 }
--- lib/features/genre_details/presentation/controllers/genre_details_controller.dart
+++ lib/features/genre_details/presentation/controllers/genre_details_controller.dart
@@ -1,3 +1,56 @@
-class GenreDetailsController{
-final GerneDetailsRepository _gerneDetailsRepository;
+import 'package:get/get.dart';
+import 'package:music_app/shared/models/music_model.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/mixins/screen_loading_and_error_mixin.dart';
+import '../../../../shared/features/music_app/presentation/controllers/music_player_controller.dart';
+import '../../../../shared/models/genre_detail_model.dart';
+import '../../../../shared/models/genre_model.dart';
+import '../../data/repositories/genre_details_repository.dart';
+
+class GenreDetailsController extends GetxController
+    with ScreenLoadingAndErrorMixin {
+  final GenreDetailsRepository _genreDetailsRepository;
+  final MusicPlayerController _musicPlayerController;
+
+  GenreDetailsController(
+      {required GenreDetailsRepository genreDetailsRepository,
+      required MusicPlayerController musicPlayerController})
+      : _genreDetailsRepository = genreDetailsRepository,
+        _musicPlayerController = musicPlayerController;
+
+  GenreDetailModel? genreDetail;
+
+  @override
+  void onInit() {
+    final genre = Get.arguments as GenreModel;
+    loadGenreDetails(genre.searchString);
+
+    super.onInit();
+  }
+
+  Future<void> loadGenreDetails(String genre) async {
+    setLoadingToTrue();
+    setError(null);
+    final genreDetailsResponseEither =
+        await _genreDetailsRepository.getGenreDetails(genre);
+
+    genreDetailsResponseEither.fold((Failure failureResponse) {
+      if (failureResponse is GetGenreDetailFailure) {
+        setError(failureResponse.message);
+      }
+    }, (GenreDetailModel genreDetailsResponse) {
+      genreDetail = genreDetailsResponse;
+
+      _musicPlayerController.loadPlayingList(
+          genreDetailsResponse.musics, _musicPlayerController.selectedPlaylist);
+    });
+
+    setLoadingToFalse();
+
+    MusicModel? getMusicByIndex(int index) => genreDetail?.musics[index];
+
+    // List<MusicModel>? get getMusic => genreDetail?.musics;
+    List<MusicModel>? getMusic = genreDetail?.musics;
+  }
 }
